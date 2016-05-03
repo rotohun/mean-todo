3f2647e7cc3206e28c10a5d765a2ed7d32381364
Ronald Hunter <ronald@ronaldhunter.co>
2016-05-03 16:55:50 -0400

    added webpack and added mock data for the angular app and hooked the express to the angular

---
+++ src/api/index.js
@@ -0,0 +1,27 @@
+'use strict'
+
+
+var express = require('express');
+var todos = require('../../mock/todos.json');
+
+
+var router = express.Router();
+
+
+router.get('/todos', function(req, res){
+
+	res.json({todos: todos});
+
+});
+
+
+
+// TODO: Add POST route to create new entries.
+
+// TODO: Add PUT route to update existing entries.
+
+// TODO: Add DELETE route to create new entries.
+
+
+
+module.exports = router
