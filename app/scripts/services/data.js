33c692a6b00205e5bec974fc64c96a2bb1dfec41
Ronald Hunter <ronald@ronaldhunter.co>
2016-05-04 16:33:35 -0400

    makes delete request to api

--- app/scripts/services/data.js
+++ app/scripts/services/data.js
@@ -3,17 +3,35 @@
 var angular = require('angular');
 
 angular.module('todoListApp')
-.service('dataService', function($http) {
+.service('dataService', function($http,$q) {
   this.getTodos = function(cb) {
     $http.get('/api/todos').then(cb);
   };
-  
-  this.deleteTodo = function(todo) {
-    console.log("I deleted the " + todo.name + " todo!");
+
+  this.deleteTodo = function(todos) {
+      console.log(todos._id);
+      return $http.delete('/api/todos/' + todos._id).then(function () {
+          console.log("I deleted the " + todos.name + " todo!");
+      });
   };
-  
+
   this.saveTodos = function(todos) {
-    console.log("I saved " + todos.length + " todos!");
+    var queue = [];
+    todos.forEach(function(todo){
+        var request;
+        if(!todo._id){
+          request = $http.post('/api/todos', todo)
+        } else {
+          request = $http.put('/api/todos/'+ todo._id, todo).then(function(results){
+            todo = results.data.todo
+            return todo;
+          })
+        };
+        queue.push(request);
+    });
+    return $q.all(queue).then(function (results) {
+      console.log("I saved " + todos.length + " todos.")
+    })
   };
-  
+
 });
