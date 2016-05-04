9d73091b5a546d07f1b453af61b5dac1963d4e2f
Ronald Hunter <ronald@ronaldhunter.co>
2016-05-04 16:32:07 -0400

    added delete request to the route

--- src/api/index.js
+++ src/api/index.js
@@ -39,6 +39,33 @@ router.post('/todos',function(req,res){
 	})
 });
 
+router.put('/todos/:id',function(req,res){
+	var id = req.params.id;
+	var todo = req.body;
+
+	if(todo && todo._id !== id){
+		return res.status(500).json({err: "Ids don't match"})
+	}
+
+	Todo.findByIdAndUpdate(id, todo, {new:true}, function(err, todo){
+			if (err) {
+				return res.status(500).json({err: err.message})
+			}
+
+			res.json({'todo': todo, message: 'Todo updated'});
+	})
+});
+
+router.delete('/todos/:id',function(req,res){
+	var todoId = req.params.id; // This maps to the :id in the url
+	Todo.findByIdAndRemove(todoId, function (err, result) {
+			if (err) {
+					res.status(500).json({ message: err.message });
+			} else {
+					res.json({ message: 'Deleted Todo' });
+			}
+	});
+});
 
 
 // TODO: Add POST route to create new entries.
