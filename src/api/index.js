92de7ab7f77e35d5f94fb6f8ce3a57cd2d81dd83
Ronald Hunter <ronald@ronaldhunter.co>
2016-05-04 13:52:33 -0400

    Created new route for posting to the todos api

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,8 @@
 
 
 var express = require('express');
-var todos = require('../../mock/todos.json');
+var Todo =  require('../models/todo');
+//var todos = require('../../mock/todos.json');
 
 
 var router = express.Router();
@@ -10,8 +11,32 @@ var router = express.Router();
 
 router.get('/todos', function(req, res){
 
-	res.json({todos: todos});
+	Todo.find({}, function(err, todos){
 
+		if (err) {
+
+
+			return res.status(500).json({message: err.message})
+		}
+		res.json({todos: todos});
+
+	});
+
+});
+
+
+
+
+router.post('/todos',function(req,res){
+
+	var todo = req.body;
+	Todo.create(todo, function(err, todo){
+			if (err) {
+				return res.status(500).json({err: err.message})
+			}
+
+			res.json({'todo': todo, message: 'Todo Created'});
+	})
 });
 
 
