ece119745994c0b129ca86993bdc29f87032a6a7
Ronald Hunter <ronald@ronaldhunter.co>
2016-05-04 16:36:27 -0400

    added todos resetstate method

--- app/scripts/controllers/todo.js
+++ app/scripts/controllers/todo.js
@@ -8,13 +8,20 @@ angular.module('todoListApp')
     $scope.todos.splice(index, 1);
     dataService.deleteTodo(todo);
   };
-  
+
   $scope.saveTodos = function() {
     var filteredTodos = $scope.todos.filter(function(todo){
       if(todo.edited) {
         return todo
       };
     })
-    dataService.saveTodos(filteredTodos);
-  }; 
+    dataService.saveTodos(filteredTodos)
+    .finally($scope.resetTodoState())
+  };
+
+  $scope.resetTodoState = function (){
+    $scope.todos.forEach(function(todo){
+      todo.edited = false;
+    })
+  }
 });
